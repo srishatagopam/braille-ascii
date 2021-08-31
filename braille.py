4384521cd2ceeda38a6547246625a671fc5edb3c
Sri Shatagopam <harsha.shatagopam@gmail.com>
2021-08-30 18:08:34 -0700

    Added comments

--- braille.py
+++ braille.py
@@ -2,8 +2,11 @@ import argparse
 import cv2 as cv
 from itertools import product
 
+# https://en.wikipedia.org/wiki/Braille_ASCII
+# Braille dot binary string corresponds to specific braille glyph:
+# Every three digits corresponds to each column of the glyph.
 bin_braille_dict = {
-    0b000000: '⠂',
+    0b000000: '⠂',      # Should be '⠀', but using spaces leads to formatting issues. For now, we use '⠂'.
     0b100000: '⠁',
     0b010000: '⠂',
     0b110000: '⠃',
@@ -71,8 +74,19 @@ bin_braille_dict = {
 
 
 def read_and_thresh(filepath, val, auto=True, inv=False):
+    '''
+    Reads an input image and converts it into a binary image.
+    :param filepath: input image filepath.
+    :param val: threshold value for creating binary image; set by user.
+    :param auto: flag on whether to use automatic thresholding.
+    :param inv: flag on whether to invert the binary image.
+    :return:
+    '''
     bgr = cv.imread(filepath)
     gray = cv.cvtColor(bgr, cv.COLOR_BGR2GRAY)
+
+    # Use Otsu's method for automatic thresholding: https://en.wikipedia.org/wiki/Otsu%27s_method
+    # Otherwise perform ordinary binary segmentation.
     if auto:
         threshold, thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU) if not inv else cv.threshold(
             gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
@@ -84,6 +98,12 @@ def read_and_thresh(filepath, val, auto=True, inv=False):
 
 
 def shave_img(img):
+    '''
+    NOTE: NOT USED CURRENTLY
+    Shaves off select rows/columns if the dimensions do not allow for the grouping of pixels into 3x2 blocks.
+    :param img: input image matrix.
+    :return: image matrix that is "cropped" to allow for clean blocking.
+    '''
     height = img.shape[0]
     width = img.shape[1]
 
@@ -96,17 +116,26 @@ def shave_img(img):
 
 
 def bin2braille(img):
+    '''
+    Accumulates braille characters in a list based on values in each pixel block.
+    :param img: input image matrix.
+    :return: list containing braille glyphs to be used for printing.
+    '''
     height = img.shape[0]
     width = img.shape[1]
 
     ascii_list = []
+    # The image matrix is segmented into 3x2 blocks.
     for blockrow, blockcol in product(range(0, height, 3), range(0, width, 2)):
         bin_str = ''
+        # Next, iterate through each pixel within the block.
         for col, row in product(range(2), range(3)):
+            # Obtain the binary string that corresponds to each glyph.
             if img[row + blockrow, col + blockcol] > 0:
                 bin_str += '1'
             else:
                 bin_str += '0'
+        # Convert the string to int and get its corresponding value from the bin_braille_dict.
         braille_char = bin_braille_dict.get(int(bin_str, 2))
         ascii_list.append(braille_char)
 
@@ -114,6 +143,14 @@ def bin2braille(img):
 
 
 def print_braille(ascii_list, width, height):
+    '''
+    Print the ASCII art given the dimensions.
+    :param ascii_list: list containing braille glyphs.
+    :param width: width of ASCII art.
+    :param height: height of ASCII art.
+    :return: nothing; print ASCII art.
+    '''
+    # Dimensions are shortened because the pixels are "grouped together" as braille glyphs.
     height //= 3
     width //= 2
     count = 0
@@ -127,6 +164,14 @@ def print_braille(ascii_list, width, height):
 
 
 def save_braille(ascii_list, width, height, outFile):
+    '''
+    Does the same as print_braille() but saves to a file.
+    :param ascii_list: list containing braille glyphs.
+    :param width: width of ASCII art.
+    :param height: height of ASCII art.
+    :param outFile: file to save ASCII art text to.
+    :return: nothing; save ASCII art.
+    '''
     height //= 3
     width //= 2
     count = 0
@@ -151,6 +196,7 @@ def main():
     parser.add_argument('--inv', dest='invFlag', required=False)
     parser.add_argument('--thresh', dest='threshVal', required=False)
 
+    # Set argument parameter defaults.
     args = parser.parse_args()
 
     filepath = args.filepath
@@ -176,6 +222,7 @@ def main():
         autoFlag = False
 
     img = read_and_thresh(filepath, threshVal, autoFlag, invFlag)
+    # 4/3 * width seemed like a good height that didn't "strech" the art too much.
     img = cv.resize(img, ((width * 4) // 3, width), interpolation=cv.INTER_AREA)
     height = img.shape[0]
     width = img.shape[1]
