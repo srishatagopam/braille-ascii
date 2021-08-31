92027ec03c01a75e7412ff1f4c91895fd3dfae1d
Sri Shatagopam <harsha.shatagopam@gmail.com>
2021-08-30 19:15:54 -0700

    Fixed some bugs

--- braille.py
+++ braille.py
@@ -1,76 +1,7 @@
 import argparse
 import cv2 as cv
 from itertools import product
-
-# https://en.wikipedia.org/wiki/Braille_ASCII
-# Braille dot binary string corresponds to specific braille glyph:
-# Every three digits corresponds to each column of the glyph.
-bin_braille_dict = {
-    0b000000: '⠂',      # Should be '⠀', but using spaces leads to formatting issues. For now, we use '⠂'.
-    0b100000: '⠁',
-    0b010000: '⠂',
-    0b110000: '⠃',
-    0b001000: '⠄',
-    0b101000: '⠅',
-    0b011000: '⠆',
-    0b111000: '⠇',
-    0b000100: '⠈',
-    0b100100: '⠉',
-    0b010100: '⠊',
-    0b110100: '⠋',
-    0b001100: '⠌',
-    0b101100: '⠍',
-    0b011100: '⠎',
-    0b111100: '⠏',
-    0b000010: '⠐',
-    0b100010: '⠑',
-    0b010010: '⠒',
-    0b110010: '⠓',
-    0b001010: '⠔',
-    0b101010: '⠕',
-    0b011010: '⠖',
-    0b111010: '⠗',
-    0b000110: '⠘',
-    0b100110: '⠙',
-    0b010110: '⠚',
-    0b110110: '⠛',
-    0b001110: '⠜',
-    0b101110: '⠝',
-    0b011110: '⠞',
-    0b111110: '⠟',
-    0b000001: '⠠',
-    0b100001: '⠡',
-    0b010001: '⠢',
-    0b110001: '⠣',
-    0b001001: '⠤',
-    0b101001: '⠥',
-    0b011001: '⠦',
-    0b111001: '⠧',
-    0b000101: '⠨',
-    0b100101: '⠩',
-    0b010101: '⠪',
-    0b110101: '⠫',
-    0b001101: '⠬',
-    0b101101: '⠭',
-    0b011101: '⠮',
-    0b111101: '⠯',
-    0b000011: '⠰',
-    0b100011: '⠱',
-    0b010011: '⠲',
-    0b110011: '⠳',
-    0b001011: '⠴',
-    0b101011: '⠵',
-    0b011011: '⠶',
-    0b111011: '⠷',
-    0b000111: '⠸',
-    0b100111: '⠹',
-    0b010111: '⠺',
-    0b110111: '⠻',
-    0b001111: '⠼',
-    0b101111: '⠽',
-    0b011111: '⠾',
-    0b111111: '⠿'
-}
+from braille_dict import get_bin_braille_dict
 
 
 def read_and_thresh(filepath, val, auto=True, inv=False):
@@ -99,8 +30,8 @@ def read_and_thresh(filepath, val, auto=True, inv=False):
 
 def shave_img(img):
     '''
-    NOTE: NOT USED CURRENTLY
-    Shaves off select rows/columns if the dimensions do not allow for the grouping of pixels into 3x2 blocks.
+    Shaves off select rows/columns if the dimensions do not allow for the grouping of pixels into 3x2 blocks. The edges
+    of the image are shaved off because it is assumed the ROI is towards the center of the image.
     :param img: input image matrix.
     :return: image matrix that is "cropped" to allow for clean blocking.
     '''
@@ -121,6 +52,11 @@ def bin2braille(img):
     :param img: input image matrix.
     :return: list containing braille glyphs to be used for printing.
     '''
+    # https://en.wikipedia.org/wiki/Braille_ASCII
+    # Braille dot binary string corresponds to specific braille glyph:
+    # Every three digits corresponds to each column of the glyph.
+    bin_braille_dict = get_bin_braille_dict()
+
     height = img.shape[0]
     width = img.shape[1]
 
@@ -165,7 +101,7 @@ def print_braille(ascii_list, width, height):
 
 def save_braille(ascii_list, width, height, outFile):
     '''
-    Does the same as print_braille() but saves to a file.
+    Does the same as print_braille() but saves to a file instead of printing.
     :param ascii_list: list containing braille glyphs.
     :param width: width of ASCII art.
     :param height: height of ASCII art.
@@ -176,7 +112,7 @@ def save_braille(ascii_list, width, height, outFile):
     width //= 2
     count = 0
 
-    file = open(outFile, 'w')
+    file = open(outFile, 'w', encoding='utf-8')
     for rows in range(height):
         line_str = ''
         sublist = ascii_list[count:count + width - 1]
@@ -193,37 +129,27 @@ def main():
     parser.add_argument('--img', dest='filepath', required=True)
     parser.add_argument('--width', dest='width', required=False)
     parser.add_argument('--save', dest='outFile', required=False)
-    parser.add_argument('--inv', dest='invFlag', required=False)
     parser.add_argument('--thresh', dest='threshVal', required=False)
+    parser.add_argument('--inv', dest='invFlag', action='store_true')
 
     # Set argument parameter defaults.
     args = parser.parse_args()
 
     filepath = args.filepath
 
-    width = 60
-    if args.width:
-        width = args.width
+    width = 2 * int(args.width) if args.width else 60
+    invFlag = args.invFlag if args.invFlag else False
 
-    outFile = 'braille_ascii_out.txt'
-    outFlag = False
-    if args.outFile:
-        outFile = args.outFile
-        outFlag = True
+    outFile = args.outFile if args.outFile else 'braille_ascii_out.txt'
+    outFlag = True if args.outFile else False
 
-    invFlag = False
-    if args.invFlag:
-        invFlag = True
-
-    threshVal = 150
-    autoFlag = True
-    if args.threshVal:
-        threshVal = args.threshVal
-        autoFlag = False
+    threshVal = int(args.threshVal) if args.threshVal else 150
+    autoFlag = False if args.threshVal else True
 
     img = read_and_thresh(filepath, threshVal, autoFlag, invFlag)
-    # 4/3 * width seemed like a good height that didn't "strech" the art too much.
-    img = cv.resize(img, ((width * 4) // 3, width), interpolation=cv.INTER_AREA)
+    # Half the width seemed like a good height that didn't "strech" the art too much.
+    img = cv.resize(img, (width, width // 2), interpolation=cv.INTER_AREA)
+    img = shave_img(img)
     height = img.shape[0]
     width = img.shape[1]
     ascii_list = bin2braille(img)
